bda83234e8d75fa075b351b2186eae970f83b7be
guslington <viney-34@hotmail.com>
2018-09-18 13:28:40 +1000

    add error sns topic

--- backup.sh
+++ backup.sh
@@ -5,17 +5,32 @@ if [ -z ${BUCKETS} ]; then
   exit 1
 fi
 
+if [ -z ${BUCKET_SUFFIX} ]; then
+  BUCKET_SUFFIX="backup"
+fi
+
+START=$SECONDS
 IFS=', ' read -r -a BUCKET_ARRAY <<< "$BUCKETS"
 
 for bucket in ${BUCKET_ARRAY[@]}; do
+
   echo "INFO: Backup for $bucket starting"
-  if aws s3 sync s3://${bucket} s3://${bucket}.backup/; then
+  if aws s3 sync s3://${bucket} s3://${bucket}.${BUCKET_SUFFIX}/; then
     STATUS="OK"
   else
     STATUS="FAILED"
   fi
-  echo "INFO: Backup completed with status $STATUS"
-  aws sns publish --topic-arn ${SNS_TOPIC} --message "{\"Operation\":\"S3BucketBackup\",\"Status\":\"${STATUS}\",\"BackupType\":\"S3\",\"Bucket\":\"$bucket\",\"Timestamp\":\"$(date +%Y%m%d-%H%M%S)\"}"
+  MESSAGE="{\"Operation\":\"S3BucketBackup\",\"Status\":\"${STATUS}\",\"BackupType\":\"S3\",\"Bucket\":\"$bucket\",\"Timestamp\":\"$(date +%Y%m%d-%H%M%S)\"}"
+  echo "INFO: Backup task finished with status $STATUS"
+
+  if [ ! -z ${SNS_ERROR_TOPIC} && $STATUS == "FAILED" ]; then
+    echo "INFO: Sending sns error notification to $SNS_ERROR_TOPIC\n$MESSAGE"
+    aws sns publish --topic-arn ${SNS_ERROR_TOPIC} --message $MESSAGE
+  elif [ ! -z ${SNS_TOPIC} ]; then
+    echo "INFO: Sending sns notification to $SNS_TOPIC\n$MESSAGE"
+    aws sns publish --topic-arn ${SNS_TOPIC} --message $MESSAGE
+  fi
+
 done
 
-echo "INFO: done!"
+echo "INFO: Backup task complete\nduration: $((SECONDS-START)) seconds elapsed.."
